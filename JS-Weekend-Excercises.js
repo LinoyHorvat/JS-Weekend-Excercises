ce9249320d496ceda61758ac549f4213b2648d6d
LinoyHorvat <linoy.horvat@gmail.com>
2021-11-12 17:55:32 +0200

    first commit

--- JS-Weekend-Excercises.js
+++ JS-Weekend-Excercises.js
@@ -53,3 +53,9 @@ function findNextSquare(num){
 // There is an array with some numbers. All numbers are equal except for one. Try to find it! findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
 // findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
 // It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(arr){
+  const dic = {}
+  arr.forEach((num) => (dic[num] = dic[num] ? ++dic[num] : 1));
+  return Object.values(dic)[0] > Object.values(dic)[1] ? Object.keys(dic)[1] : Object.keys(dic)[0];
+}
